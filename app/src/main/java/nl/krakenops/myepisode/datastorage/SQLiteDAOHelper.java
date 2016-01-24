9fa040cb0526a8f493e458b4152d5dc61493b2f2
Matthijs Overboom <matthijsske@gmail.com>
2016-01-24 14:59:13 +0100

    Created SQLiteDAOHelper class

---
+++ app/src/main/java/nl/krakenops/myepisode/datastorage/SQLiteDAOHelper.java
@@ -0,0 +1,96 @@
+package nl.krakenops.myepisode.datastorage;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.io.Serializable;
+
+import nl.krakenops.myepisode.model.Thumbnail;
+
+/**
+ * Created by Matthijs on 24/01/2016.
+ */
+public class SQLiteDAOHelper extends SQLiteOpenHelper implements Serializable {
+    //Static constants which will be used throughout the class.
+    //Database vars
+    public static final String DATABASE_NAME = "MyEpisodeShows";
+    protected static final int DATABASE_VERSION = 1;
+
+    //Tables
+    protected static final String TABLE_SHOWS = "shows";
+    protected static final String TABLE_EPISODE = "episode";
+    protected static final String TABLE_SEASON = "season";
+    protected static final String TABLE_THUMBNAIL = "thumbnail";
+    protected static final String TABLE_BACKDROP = "backdrop";
+
+    //Columns
+    protected static final String COL_ID = "ID";
+
+    protected static final String COL_THUMBNAILPATH = "thumbnailPath";
+    protected static final String COL_NAME = "name";
+    protected static final String COL_LASTWATCHED = "lastWatched";
+    protected static final String COL_NEXTEPISODEAIRS = "nextEpisodeAirs";
+    protected static final String COL_FKTHUMBNAILID = "FK_ThumbnailID";
+    protected static final String COL_BACKDROPPATH = "backdropPath";
+    protected static final String COL_SEASON = "season";
+    protected static final String COL_MAXEPISODES = "maxEpisodes";
+    protected static final String COL_FKBACKDROPID = "FK_BackdropID";
+    protected static final String COL_FKSHOWID = "FK_ShowID";
+    protected static final String COL_EPISODE = "episode";
+    protected static final String COL_WATCHEDAT = "watchedAt";
+    protected static final String COL_FKSEASONID = "FK_SeasonID";
+
+    public SQLiteDAOHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //table which holds thumbnails with backdrop
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_THUMBNAIL +
+                "("+COL_ID+" INTEGER primary key autoincrement, " + COL_THUMBNAILPATH + "VARCHAR NOT NULL);");
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_SHOWS +
+                "("+COL_ID+" INTEGER primary key autoincrement, " +
+                COL_NAME + "VARCHAR NOT NULL, " +
+                COL_LASTWATCHED + "TIMESTAMP DEFAULT CURRENT_TIMESTAMP, " +
+                COL_NEXTEPISODEAIRS + "TIMESTAMP, " +
+                COL_FKTHUMBNAILID + "INTEGER," +
+                "FOREIGN KEY("+COL_FKTHUMBNAILID+") REFERENCES "+TABLE_THUMBNAIL+"("+COL_ID+"));");
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_BACKDROP +
+                "("+COL_ID+" INTEGER primary key autoincrement, " +
+                COL_BACKDROPPATH + "VARCHAR);");
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_SEASON +
+                "(" + COL_ID + " INTEGER primary key autoincrement, " +
+                COL_SEASON + "INTEGER NOT NULL, " +
+                COL_MAXEPISODES + "INTEGER, " +
+                COL_FKBACKDROPID + "INTEGER, " +
+                COL_FKSHOWID + "INTEGER, " +
+                "FOREIGN KEY(" + COL_FKBACKDROPID + ") REFERENCES " + TABLE_BACKDROP + "(" + COL_ID + ")" +
+                "FOREIGN KEY(" + COL_FKSHOWID + ") REFERENCES shows(" + COL_ID + "));");
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_EPISODE +
+                "(" + COL_ID + " INTEGER primary key autoincrement, " +
+                COL_EPISODE + "INTEGER NOT NULL, " +
+                COL_WATCHEDAT + "TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
+                COL_FKSEASONID + "INTEGER NOT NULL, " +
+                "FOREIGN KEY(" + COL_FKSEASONID + ") REFERENCES " + TABLE_SEASON + "(" + COL_ID + "));");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String query;
+        query = "DROP TABLE IF EXISTS episode; " +
+                "DROP TABLE IF EXISTS season; " +
+                "DROP TABLE IF EXISTS backdrop; " +
+                "DROP TABLE IF EXISTS shows " +
+                "DROP TABLE IF EXISTS thumbnail;";
+        db.execSQL(query);
+        onCreate(db);
+        db.close();
+    }
+}
--- app/src/main/java/nl/krakenops/myepisode/datastorage/SQLiteShowDAO.java
+++ app/src/main/java/nl/krakenops/myepisode/datastorage/SQLiteShowDAO.java
@@ -3,9 +3,11 @@ package nl.krakenops.myepisode.datastorage;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import java.io.Serializable;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,100 +17,35 @@ import nl.krakenops.myepisode.model.Thumbnail;
 /**
  * Created by Matthijs on 22/01/2016.
  */
-public class SQLiteShowDAO extends SQLiteOpenHelper implements Serializable, ShowDAOInf {
-    //Static constants which will be used throughout the class.
-    //Database vars
-    private static final String DATABASE_NAME = "MyEpisodeShows";
-    private static final int DATABASE_VERSION = 1;
-
-    //Tables
-    private static final String TABLE_SHOWS = "shows";
-    private static final String TABLE_EPISODE = "episode";
-    private static final String TABLE_SEASON = "season";
-    private static final String TABLE_THUMBNAIL = "thumbnail";
-    private static final String TABLE_BACKDROP = "backdrop";
-
-    //Columns
-    private static final String COL_ID = "ID";
-
-    private static final String COL_THUMBNAILPATH = "thumbnailPath";
-    private static final String COL_NAME = "name";
-    private static final String COL_LASTWATCHED = "lastWatched";
-    private static final String COL_NEXTEPISODEAIRS = "nextEpisodeAirs";
-    private static final String COL_FKTHUMBNAILID = "FK_ThumbnailID";
-    private static final String COL_BACKDROPPATH = "backdropPath";
-    private static final String COL_SEASON = "season";
-    private static final String COL_MAXEPISODES = "maxEpisodes";
-    private static final String COL_FKBACKDROPID = "FK_BackdropID";
-    private static final String COL_FKSHOWID = "FK_ShowID";
-    private static final String COL_EPISODE = "episode";
-    private static final String COL_WATCHEDAT = "watchedAt";
-    private static final String COL_FKSEASONID = "FK_SeasonID";
+public class SQLiteShowDAO implements Serializable, ShowDAOInf {
+    private SQLiteDatabase db;
+    private SQLiteDAOHelper dbHelper;
 
     public SQLiteShowDAO(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        dbHelper = new SQLiteDAOHelper(context);
     }
 
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        //table which holds thumbnails with backdrop
-        db.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_THUMBNAIL +
-                "("+COL_ID+" INTEGER primary key autoincrement, " + COL_THUMBNAILPATH + "VARCHAR NOT NULL);");
-
-        db.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_SHOWS +
-                "("+COL_ID+" INTEGER primary key autoincrement, " +
-                COL_NAME + "VARCHAR NOT NULL, " +
-                COL_LASTWATCHED + "TIMESTAMP DEFAULT CURRENT_TIMESTAMP, " +
-                COL_NEXTEPISODEAIRS + "TIMESTAMP, " +
-                COL_FKTHUMBNAILID + "INTEGER," +
-                "FOREIGN KEY("+COL_FKTHUMBNAILID+") REFERENCES "+TABLE_THUMBNAIL+"("+COL_ID+"));");
-
-        db.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_BACKDROP +
-                "("+COL_ID+" INTEGER primary key autoincrement, " +
-                COL_BACKDROPPATH + "VARCHAR);");
-
-        db.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_SEASON +
-                "("+COL_ID+" INTEGER primary key autoincrement, " +
-                COL_SEASON+ "INTEGER NOT NULL, " +
-                COL_MAXEPISODES + "INTEGER, " +
-                COL_FKBACKDROPID + "INTEGER, " +
-                COL_FKSHOWID + "INTEGER, " +
-                "FOREIGN KEY("+COL_FKBACKDROPID+") REFERENCES "+TABLE_BACKDROP+"("+COL_ID+")" +
-                "FOREIGN KEY("+COL_FKSHOWID+") REFERENCES shows("+COL_ID+"));");
-
-        db.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_EPISODE +
-                "("+COL_ID+" INTEGER primary key autoincrement, " +
-                COL_EPISODE + "INTEGER NOT NULL, " +
-                COL_WATCHEDAT + "TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
-                COL_FKSEASONID + "INTEGER NOT NULL, " +
-                "FOREIGN KEY("+COL_FKSEASONID+") REFERENCES "+TABLE_SEASON+"("+COL_ID+"));");
+    public void open() throws SQLException {
+        db = dbHelper.getWritableDatabase();
     }
 
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        String query;
-        query = "DROP TABLE IF EXISTS episode; " +
-                "DROP TABLE IF EXISTS season; " +
-                "DROP TABLE IF EXISTS backdrop; " +
-                "DROP TABLE IF EXISTS shows " +
-                "DROP TABLE IF EXISTS thumbnail;";
-        db.execSQL(query);
-        onCreate(db);
-        db.close();
+    public void close() {
+        dbHelper.close();
     }
 
     @Override
     public boolean insertShow(Thumbnail thumbnail) {
-        SQLiteDatabase db = this.getWritableDatabase();
-
+        boolean result = false;
         //Creating ContentValue pairs
-        ContentValues values = new ContentValues();
-        values.put(COL_NAME, thumbnail.getName());
+//        ContentValues values = new ContentValues();
+//        values.put(dbHelper.COL_NAME, thumbnail.getName());
+//
+//        // Inserting Row
+//        db.insert(dbHelper.TABLE_SHOWS, null, values);
+//        db.close();
 
-        // Inserting Row
-        db.insert(TABLE_SHOWS, null, values);
-        db.close();
-        return false;
+
+        return result;
     }
 
     @Override
